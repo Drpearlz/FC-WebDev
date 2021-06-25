833bf58d80112adbf5afe81c80f4068d6f9167cf
Peggy Roxy <roxypeggy43@gmail.com>
2021-06-24 23:07:01 -0700

    first responsive tutorial portfolio, still needs work and pics

--- PracticeWS/script.js
+++ PracticeWS/script.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
       $('header').toggleClass('toggle');
     })
    })
-  
+   function myFunction() {
+    var txt;
+    var person = prompt("Please enter your name:", "Jay Doe");
+    if (person == null || person == "") {
+      txt = "";
+    } else {
+      txt = "Hello " + person + "!\nHow are you today?";
+    }
+    document.getElementById("demo").innerHTML = txt;
+  };
